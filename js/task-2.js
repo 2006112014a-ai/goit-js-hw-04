a764b5cd17de2d3e97ccbdecf3cab5ba242582cc
2006112014a-ai <2006112014a@gmail.com>
2026-09-12 17:27:42 +0300

    Add empty array example

--- js/task-2.js
+++ js/task-2.js
@@ -17,21 +17,16 @@ console.log(
     { day: "monday", calories: 3010 },
     { day: "tuesday", calories: 3200 },
     { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 2870 },
-    { day: "sunday", calories: 3180 },
   ])
-); // 3104.285714285714
+);
 
 console.log(
   calcAverageCalories([
     { day: "monday", calories: 2040 },
     { day: "tuesday", calories: 2270 },
     { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 },
   ])
-); // 2269.9999999999995
+);
+
+// Перевірка порожнього масиву
+console.log(calcAverageCalories([])); // 0
